4115db09fa2e9bcf90d365d72ee399fc5665d33f
iabreuda <igorabreu.ds@gmail.com>
2016-12-09 09:15:18 -0200

    Inicio do Trab Final 2016_2

--- Questa1f.m
+++ Questa1f.m
@@ -2,21 +2,21 @@ clear all;
 close all;
 
 figure;
-h1 = tf([0 100 0], [1000 110 110]);
+h1 = tf([0 0.000166 0], [0.00000996 0.000249 500000]);
 bode(h1);
-
-figure;
-h2 = tf([0 10 0], [1 110 1]);
-bode(h2);
-
-figure;
-h3 = tf([1 0 0], [2000 110 1]);
-bode(h3);
-
-figure;
-h4 = tf([0 0 1], [0 0 1]);
-bode(h4);
-
-figure;
-h5 = tf([0 0 1], [0 -10 0]);
-bode(h5);
+% 
+% figure;
+% h2 = tf([0 10 0], [1 110 1]);
+% bode(h2);
+% 
+% figure;
+% h3 = tf([1 0 0], [2000 110 1]);
+% bode(h3);
+% 
+% figure;
+% h4 = tf([0 0 1], [0 0 1]);
+% bode(h4);
+% 
+% figure;
+% h5 = tf([0 0 1], [0 -10 0]);
+% bode(h5);
