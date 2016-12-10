60076dee490ef2b27cdc7b62580a4977bcb8a9c7
iabreuda <igorabreu.ds@gmail.com>
2016-12-09 23:52:47 -0200

    Final

--- Questa1h.m
+++ Questa1h.m
@@ -4,17 +4,17 @@ close all;
 s = tf('s');
 
 figure;
-h1 = tf([0 100 0], [1000 110 110]);
+h1 = tf([0 2.32 0], [0.000285824 3.6192 5600]);
 step(h1/s);
 title('Resposta a rampa');
 
 figure;
-h2 = tf([0 10 0], [1 110 1]);
+h2 = tf([0 2.2 0], [0.000000051 2.20022 1]);
 step(h2/s);
 title('Resposta a rampa');
 
 figure;
-h3 = tf([1 0 0], [2000 110 1]);
+h3 = tf([0 0 1], [0.000000025 0.00055 1]);
 step(h3/s);
 title('Resposta a rampa');
 
@@ -24,6 +24,6 @@ step(h4/s);
 title('Resposta a rampa');
 
 figure;
-h5 = tf([0 0 1], [0 -10 0]);
+h5 = tf([0 0 1], [0 -0.05 0]);
 step(h5/s);
 title('Resposta a rampa');
